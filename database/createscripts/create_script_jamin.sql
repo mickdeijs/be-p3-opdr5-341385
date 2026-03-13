c8da25096e3e1e676cc873ad8b7f7c818d80b098
Mick Deijs <341385@student.mboutrecht.nl>
2026-03-13 13:28:01 +0100

    verder vanaf opdr 4

---
+++ database/createscripts/create_script_jamin.sql
@@ -0,0 +1,394 @@
+-- Step: 01
+-- Goal: Create a new database jamin_io_sd_2024a
+-- **********************************************************************************
+-- Version       Date:           Author:                     Description:
+-- *******       **********      ****************            ******************
+-- 01            12-09-2025      Arjan de Ruijter            New Database
+-- **********************************************************************************/
+
+-- Use database jamin_io_sd_2024a
+Use `BE-p3`;
+
+
+-- Step: 02
+-- Goal: Create a new table Allergeen
+-- **********************************************************************************
+-- Version       Date:           Author:                     Description:
+-- *******       **********      ****************            ******************
+-- 01            12-09-2025      Arjan de Ruijter            New Tabel
+-- 02            07-01-2026      Arjan de Ruijter            Fix drop order
+-- **********************************************************************************/
+
+DROP TABLE IF EXISTS ProductPerAllergeen;
+DROP TABLE IF EXISTS Allergeen;
+
+CREATE TABLE IF NOT EXISTS Allergeen
+(
+    Id              SMALLINT         UNSIGNED       NOT NULL    AUTO_INCREMENT
+   ,Naam            VARCHAR(30)                     NOT NULL
+   ,Omschrijving    VARCHAR(100)                    NOT NULL
+   ,IsActief        BIT                             NOT NULL    DEFAULT 1
+    ,Opmerking       VARCHAR(250)                        NULL    DEFAULT NULL
+    ,DatumAangemaakt DateTime(6)                     NOT NULL    DEFAULT CURRENT_TIMESTAMP(6)
+    ,DatumGewijzigd  DateTime(6)                     NOT NULL    DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+    ,CONSTRAINT      PK_Allergeen_Id   PRIMARY KEY (Id)
+) ENGINE=InnoDB;
+
+
+-- Step: 03
+-- Goal: Fill table Allergeen with data
+-- **********************************************************************************
+
+-- Version       Date:           Author:                     Description:
+-- *******       **********      ****************            ******************
+-- 01            12-09-2025      Arjan de Ruijter            New Data
+-- **********************************************************************************/
+
+INSERT INTO Allergeen
+(
+     Naam
+    ,Omschrijving
+)
+VALUES
+     ('Gluten', 'Dit product bevat gluten')
+    ,('Gelatine', 'Dit product bevat gelatine')
+    ,('AZO-kleurstof', 'Dit product bevat AZO-kleurstof')
+    ,('Lactose', 'Dit product bevat lactose')
+    ,('Soja', 'Dit product bevat soja');
+
+
+
+-- Step 04:
+-- Goal: Create a new table Product
+-- **************************************************************
+-- Version:       Date:       Author:           Description
+-- ********       ****        *******           ***********
+-- 01             18-10-2024  Arjan de Ruijter  New table
+-- 02             10-10-2025  Arjan de Ruijter  Default aangepast
+-- 03             07-01-2026  Arjan de Ruijter  Fix drop order
+-- **************************************************************
+DROP TABLE IF EXISTS ProductPerLeverancier;
+DROP TABLE IF EXISTS Magazijn;
+DROP TABLE IF EXISTS ProductPerAllergeen;
+DROP TABLE IF EXISTS Product;
+
+CREATE TABLE IF NOT EXISTS Product
+(
+     Id              MEDIUMINT             UNSIGNED        NOT NULL      AUTO_INCREMENT
+    ,Naam            VARCHAR(255)                          NOT NULL
+    ,Barcode         VARCHAR(13)                           NOT NULL
+    ,IsActief        BIT                                   NOT NULL      DEFAULT 1
+    ,Opmerking       VARCHAR(250)                              NULL      DEFAULT NULL
+    ,DatumAangemaakt Datetime(6)                           NOT NULL      DEFAULT CURRENT_TIMESTAMP(6)
+    ,DatumGewijzigd  Datetime(6)                           NOT NULL      DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+    ,CONSTRAINT      PK_Product_Id        PRIMARY KEY (Id)
+) ENGINE=InnoDB   AUTO_INCREMENT=1;
+
+
+-- Step: 05
+-- Goal: Fill table Product with data
+-- ***********************************************************
+-- Version:       Date:       Author:           Description
+-- ********       ****        *******           ***********
+-- 01             18-10-2024  Arjan de Ruijter  Insert Records
+-- ***********************************************************
+
+INSERT INTO Product
+(
+     Naam
+    ,Barcode
+)
+VALUES
+     ('Mintnopjes', '8719587231278')
+    ,('Schoolkrijt', '8719587326713')
+    ,('Honingdrop', '8719587327836')
+    ,('Zure Beren', '8719587321441')
+    ,('Cola Flesjes', '8719587321237')
+    ,('Turtles', '8719587322245')
+    ,('Witte Muizen', '8719587328256')
+    ,('Reuzen Slangen', '8719587325641')
+    ,('Zoute Rijen', '8719587322739')
+    ,('Winegums', '8719587327527')
+    ,('Drop Munten', '8719587322345')
+    ,('Kruis Drop', '8719587322265')
+    ,('Zoute Ruitjes', '8719587323256')
+    ,("Drop ninja\'s", '8719587323277');
+
+-- Update Winegums to inactive (not produced anymore)
+UPDATE Product SET IsActief = 0 WHERE Id = 10;
+
+
+-- Step 06:
+-- Goal: Create a new table Magazijn
+-- ********************************************************
+-- Version:       Date:       Author:           Description
+-- ********       ****        *******           ***********
+-- 01             18-10-2024  Arjan de Ruijter  New table
+-- ********************************************************
+
+CREATE TABLE IF NOT EXISTS Magazijn
+(
+     Id                   MEDIUMINT       UNSIGNED          NOT NULL      AUTO_INCREMENT
+    ,ProductId            MEDIUMINT       UNSIGNED          NOT NULL
+    ,VerpakkingsEenheid   DECIMAL(6,3)                      NOT NULL
+    ,AantalAanwezig       INT             UNSIGNED          NULL
+    ,IsActief             BIT                               NOT NULL      DEFAULT 1
+    ,Opmerking            VARCHAR(250)                          NULL      DEFAULT NULL
+    ,DatumAangemaakt      Datetime(6)                       NOT NULL      DEFAULT CURRENT_TIMESTAMP(6)
+    ,DatumGewijzigd       Datetime(6)                       NOT NULL      DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+    ,CONSTRAINT           PK_Magazijn_Id                    PRIMARY KEY (Id)
+    ,CONSTRAINT           FK_Magazijn_ProductId_Product_Id  FOREIGN KEY (ProductId) REFERENCES Product(Id)
+) ENGINE=InnoDB   AUTO_INCREMENT=1;
+
+-- Step: 07
+-- Goal: Fill table Magazijn with data
+-- ***********************************************************
+-- Version:       Date:       Author:           Description
+-- ********       ****        ****************  ***********
+-- 01             18-10-2024  Arjan de Ruijter  Insert Records
+-- ***********************************************************
+
+INSERT INTO Magazijn
+(
+     ProductId
+    ,VerpakkingsEenheid
+    ,AantalAanwezig
+)
+VALUES
+     (1, 5, 453)
+    ,(2, 2.5, 400)
+    ,(3, 5, 1)
+    ,(4, 1, 800)
+    ,(5, 3, 234)
+    ,(6, 2, 345)
+    ,(7, 1, 795)
+    ,(8, 10, 233)
+    ,(9, 2.5, 123)
+    ,(10, 3, NULL)
+    ,(11, 2, 367)
+    ,(12, 1, 467)
+    ,(13, 5, 20)
+    ,(14, 1, 0);
+
+
+-- Step 08:
+-- Goal: Create a new table Contact
+-- ********************************************************
+-- Version:       Date:       Author:           Description
+-- ********       ****        *******           ***********
+-- 01             07-01-2026  Arjan de Ruijter  New table
+-- ********************************************************
+
+DROP TABLE IF EXISTS ProductPerLeverancier;
+DROP TABLE IF EXISTS Leverancier;
+DROP TABLE IF EXISTS Contact;
+
+CREATE TABLE IF NOT EXISTS Contact
+(
+     Id                 SMALLINT             UNSIGNED        NOT NULL      AUTO_INCREMENT
+    ,Straat             VARCHAR(100)                         NOT NULL
+    ,Huisnummer         VARCHAR(10)                          NOT NULL
+    ,Postcode           VARCHAR(10)                          NOT NULL
+    ,Stad               VARCHAR(60)                          NOT NULL
+    ,IsActief           BIT                                  NOT NULL      DEFAULT 1
+    ,Opmerking          VARCHAR(250)                             NULL      DEFAULT NULL
+    ,DatumAangemaakt Datetime(6)                             NOT NULL      DEFAULT CURRENT_TIMESTAMP(6)
+    ,DatumGewijzigd  Datetime(6)                             NOT NULL      DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+    ,CONSTRAINT      PK_Contact_Id        PRIMARY KEY (Id)
+) ENGINE=InnoDB   AUTO_INCREMENT=1;
+
+
+-- Step 08b:
+-- Goal: Fill table Contact with data
+-- ***********************************************************
+-- Version:       Date:       Author:           Description
+-- ********       ****        ****************  ***********
+-- 01             07-01-2026  Arjan de Ruijter  Insert Records
+-- ***********************************************************
+
+INSERT INTO Contact
+(
+     Straat
+    ,Huisnummer
+    ,Postcode
+    ,Stad
+)
+VALUES
+     ('Van Gilslaan', '34', '1045CB', 'Hilvarenbeek')
+    ,('Den Dolderpad', '2', '1067RC', 'Utrecht')
+    ,('Fredo Raalteweg', '257', '1236OP', 'Nijmegen')
+    ,('Bertrand Russellhof', '21', '2034AP', 'Den Haag')
+    ,('Leon van Bonstraat', '213', '145XC', 'Lunteren')
+    ,('Bea van Lingenlaan', '234', '2197FG', 'Sint Pancras');
+
+
+-- Step 09:
+-- Goal: Create a new table Leverancier
+-- ********************************************************
+-- Version:       Date:       Author:           Description
+-- ********       ****        *******           ***********
+-- 01             25-10-2024  Arjan de Ruijter  New table
+-- 02             07-01-2026  Arjan de Ruijter  Add ContactId FK
+-- ********************************************************
+
+CREATE TABLE IF NOT EXISTS Leverancier
+(
+     Id                 SMALLINT             UNSIGNED        NOT NULL      AUTO_INCREMENT
+    ,Naam               VARCHAR(60)                          NOT NULL
+    ,Contactpersoon     VARCHAR(60)                          NOT NULL
+    ,Leveranciernummer  VARCHAR(11)                          NOT NULL
+    ,Mobiel             VARCHAR(11)                          NOT NULL
+    ,ContactId          SMALLINT             UNSIGNED            NULL
+    ,IsActief           BIT                                  NOT NULL      DEFAULT 1
+    ,Opmerking          VARCHAR(250)                             NULL      DEFAULT NULL
+    ,DatumAangemaakt Datetime(6)                             NOT NULL      DEFAULT CURRENT_TIMESTAMP(6)
+    ,DatumGewijzigd  Datetime(6)                             NOT NULL      DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+    ,CONSTRAINT      PK_Levrancier_Id        PRIMARY KEY (Id)
+    ,CONSTRAINT      FK_Leverancier_ContactId_Contact_Id  FOREIGN KEY (ContactId) REFERENCES Contact (Id)
+) ENGINE=InnoDB   AUTO_INCREMENT=1;
+
+
+-- Step: 09b
+-- Goal: Fill table Levrancier with data
+-- ***********************************************************
+-- Version:       Date:       Author:           Description
+-- ********       ****        ****************  ***********
+-- 01             25-10-2024  Arjan de Ruijter  Insert Records
+-- 02             07-01-2026  Arjan de Ruijter  Add ContactId values
+-- ***********************************************************
+
+INSERT INTO Leverancier
+(
+     Naam
+    ,Contactpersoon
+    ,Leveranciernummer
+    ,Mobiel
+    ,ContactId
+)
+VALUES
+     ('Venco', 'Bert van Linge', 'L1029384719', '06-28493827', 1)
+    ,('Astra Sweets', 'Jasper del Monte', 'L1029284315', '06-39398734', 2)
+    ,('Haribo', 'Sven Stalman', 'L1029324748', '06-24383291', 3)
+    ,('Basset', 'Joyce Stelterberg', 'L1023845773', '06-48293823', 4)
+    ,('De Bron', 'Remco Veenstra', 'L1023857736', '06-34291234', 5)
+    ,('Quality Street', 'Johan Nooij', 'L1029234586', '06-23458456', 6)
+    ,('Hom Ken Food', 'Hom Ken', 'L1029234599', '06-23458477', NULL);
+
+
+
+-- Step 10:
+-- Goal: Create a new table ProductPerLeverancier
+-- ********************************************************
+-- Version:       Date:       Author:           Description
+-- ********       ****        *******           ***********
+-- 01             25-10-2024  Arjan de Ruijter  New table
+-- ********************************************************
+
+CREATE TABLE IF NOT EXISTS ProductPerLeverancier
+(
+     Id                             MEDIUMINT       UNSIGNED          NOT NULL      AUTO_INCREMENT
+    ,LeverancierId                  SMALLINT        UNSIGNED          NOT NULL
+    ,ProductId                      MEDIUMINT       UNSIGNED          NOT NULL
+    ,DatumLevering                  DATE                              NOT NULL
+    ,Aantal                         INT             UNSIGNED          NOT NULL
+    ,DatumEerstVolgendeLevering     DATE                                  NULL
+    ,IsActief                       BIT                               NOT NULL      DEFAULT 1
+    ,Opmerking                      VARCHAR(250)                          NULL      DEFAULT NULL
+    ,DatumAangemaakt                Datetime(6)                       NOT NULL      DEFAULT CURRENT_TIMESTAMP(6)
+    ,DatumGewijzigd                 Datetime(6)                       NOT NULL      DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+    ,CONSTRAINT                     PK_ProductPerLeverancier_Id       PRIMARY KEY (Id)
+    ,CONSTRAINT                     FK_ProductPerLeverancier_LeverancierId_Leverancier_Id  FOREIGN KEY (LeverancierId) REFERENCES Leverancier (Id)
+    ,CONSTRAINT                     FK_ProductPerLeverancier_ProductId_Product_Id  FOREIGN KEY (ProductId) REFERENCES Product (Id)
+) ENGINE=InnoDB   AUTO_INCREMENT=1;
+
+
+
+-- Step: 11
+-- Goal: Fill table ProductPerLeverancier with data
+-- ***********************************************************
+-- Version:       Date:       Author:           Description
+-- ********       ****        ****************  ***********
+-- 01             25-10-2024  Arjan de Ruijter  Insert Records
+-- ***********************************************************
+
+INSERT INTO ProductPerLeverancier
+(
+     LeverancierId
+    ,ProductID
+    ,DatumLevering
+    ,Aantal
+    ,DatumEerstVolgendeLevering
+)
+VALUES
+ (1, 1, '2024-10-09', 23, '2024-10-16')
+,(1, 1, '2024-10-18', 21, '2024-10-25')
+,(1, 2, '2024-10-09', 12, '2024-10-16')
+,(1, 3, '2024-10-10', 11, '2024-10-17')
+,(2, 4, '2024-10-14', 16, '2024-10-21')
+,(2, 4, '2024-10-21', 23, '2024-10-28')
+,(2, 5, '2024-10-14', 45, '2024-10-21')
+,(2, 6, '2024-10-14', 30, '2024-10-21')
+,(3, 7, '2024-10-12', 12, '2024-10-19')
+,(3, 7, '2024-10-19', 23, '2024-10-26')
+,(3, 8, '2024-10-10', 12, '2024-10-17')
+,(3, 9, '2024-10-11', 1, '2024-10-18')
+,(4, 10, '2024-10-16', 24, '2024-10-30')
+,(5, 11, '2024-10-10', 47, '2024-10-17')
+,(5, 11, '2024-10-19', 60, '2024-10-26')
+,(5, 12, '2024-10-11', 45, NULL)
+,(7, 14, '2023-04-14', 20, NULL)
+,(5, 13, '2024-10-12', 23, NULL);
+
+
+-- Step 12:
+-- Goal: Create a new table ProductPerAllergeen
+-- ********************************************************
+-- Version:       Date:       Author:           Description
+-- ********       ****        *******           ***********
+-- 01             19-11-2024  Arjan de Ruijter  New table
+-- ********************************************************
+
+CREATE TABLE IF NOT EXISTS ProductPerAllergeen
+(
+     Id                             MEDIUMINT       UNSIGNED          NOT NULL      AUTO_INCREMENT
+    ,ProductId                      MEDIUMINT       UNSIGNED          NOT NULL
+    ,AllergeenId                    SMALLINT        UNSIGNED          NOT NULL
+    ,IsActief                       BIT                               NOT NULL      DEFAULT 1
+    ,Opmerking                      VARCHAR(250)                          NULL      DEFAULT NULL
+    ,DatumAangemaakt                Datetime(6)                       NOT NULL      DEFAULT CURRENT_TIMESTAMP(6)
+    ,DatumGewijzigd                 Datetime(6)                       NOT NULL      DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+    ,CONSTRAINT           PK_ProductPerAllergeen_Id  PRIMARY KEY (Id)
+    ,CONSTRAINT           FK_ProductPerAllergeen_ProductId_Product_Id  FOREIGN KEY (ProductId) REFERENCES Product (Id)
+    ,CONSTRAINT           FK_ProductPerAllergeen_AllergeenId_Allergeen_Id  FOREIGN KEY (AllergeenId) REFERENCES Allergeen (Id)
+) ENGINE=InnoDB   AUTO_INCREMENT=1;
+
+
+
+-- Step: 13
+-- Goal: Fill table ProductPerAllergeen with data
+-- ***********************************************************
+-- Version:       Date:       Author:           Description
+-- ********       ****        ****************  ***********
+-- 01             19-11-2024  Arjan de Ruijter  Insert Records
+-- ***********************************************************
+
+INSERT INTO ProductPerAllergeen
+(
+     ProductId
+    ,AllergeenId
+)
+VALUES
+  (1, 2)
+ ,(1, 1)
+ ,(1, 3)
+ ,(3, 4)
+ ,(6, 5)
+ ,(9, 2)
+ ,(9, 5)
+ ,(10, 2)
+ ,(12, 4)
+ ,(13, 1)
+ ,(14, 5)
+ ,(13, 4)
+ ,(13, 5);
+
---
+++ routes/web.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
+use Livewire\Volt\Volt;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\LeverancierController;
+use App\Http\Controllers\AllergeenController;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::get('/products', [ProductController::class, 'index'])->name('product.index');
+
+Route::get('/product/{id}/allergenenInfo', [ProductController::class, 'allergenenInfo'])->name('product.allergenenInfo');
+
+Route::get('/product/{id}/leverantieInfo', [ProductController::class, 'leverantieInfo'])->name('product.leverantieInfo');
+
+// Suppliers
+Route::get('/suppliers', [SupplierController::class, 'index'])->name('suppliers.index');
+Route::get('/suppliers/{id}/products', [SupplierController::class, 'products'])->name('suppliers.products');
+
+// Delivery create/store
+Route::get('/suppliers/{supplierId}/product/{productId}/delivery', [SupplierController::class, 'createDelivery'])->name('suppliers.delivery');
+Route::post('/suppliers/{supplierId}/product/{productId}/delivery', [SupplierController::class, 'storeDelivery'])->name('suppliers.delivery.store');
+
+// Wijzigen Leveranciers
+Route::get('/leveranciers', [LeverancierController::class, 'index'])->name('leveranciers.index');
+Route::get('/leveranciers/{id}/details', [LeverancierController::class, 'details'])->name('leveranciers.details');
+Route::get('/leveranciers/{id}/edit', [LeverancierController::class, 'edit'])->name('leveranciers.edit');
+Route::put('/leveranciers/{id}', [LeverancierController::class, 'update'])->name('leveranciers.update');
+
+// Overzicht Allergenen
+Route::get('/allergenen', [AllergeenController::class, 'index'])->name('allergeen.index');
+Route::get('/allergenen/product/{id}/leverancier', [AllergeenController::class, 'leverancierInfo'])->name('allergeen.leverancier');
+
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
+    Volt::route('settings/password', 'settings.password')->name('user-password.edit');
+    Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
+
+    Volt::route('settings/two-factor', 'settings.two-factor')
+        ->middleware(
+            when(
+                Features::canManageTwoFactorAuthentication()
+                    && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
+                ['password.confirm'],
+                [],
+            ),
+        )
+        ->name('two-factor.show');
+});
